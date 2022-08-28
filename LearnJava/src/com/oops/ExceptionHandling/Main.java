6d2b25d9fd7b30fb16c7b929ce7a3b8d38dd65b2
Utkarsh Agrawal <agrawalutkarsh1906@gmail.com>
2022-08-29 00:35:26 +0530

    Learnt Exception handling and its keywords

---
+++ LearnJava/src/com/oops/ExceptionHandling/Main.java
@@ -0,0 +1,28 @@
+package com.oops.ExceptionHandling;
+
+/** There are five keywords of exception handling
+ *   1) try
+ *   2) catch
+ *   3) finally
+ *   4) throw
+ *   5) throws */
+public class Main {
+    public static void main(String[] args) {
+        int a=4;
+        int b=0;
+        try{
+            //int c=a/b;
+            System.out.println(divide(a,b));
+        }catch(ArithmeticException e){
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println("This keyword 'finally' will always run");
+        }
+    }
+    static int divide(int a,int b) throws ArithmeticException{
+        if(b==0){
+            throw new ArithmeticException("Do not divide by 0");
+        }
+        return a/b;
+    }
+}
